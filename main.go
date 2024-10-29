840606a68c0d37a6bca20635f9ed504e833876e0
Ilyas <ilyas@MacBook-Air-Ilyas.local>
2024-10-29 14:41:52 +0300

    writeMessage

--- main.go
+++ main.go
@@ -1,31 +1,33 @@
 package main
 
 import (
-	"fmt"
 	//"fmt"
 	"platform/config"
 	"platform/logging"
 )
 
-func writeMessage(logger logging.Logger) {
-	logger.Info("Hello, Platform")
-}
-func main() {
-	var logger logging.Logger = logging.NewDefaultLogger(logging.Information)
-	writeMessage(logger)
-	config := config.DefaultConfig{
-		ConfigData: map[string]interface{}{
-			"database": map[string]interface{}{
-				"host": "localhost",
-				"port": 5432,
-				"credentials": map[string]interface{}{
-					"username": "user",
-					"password": "pass",
-				},
-			},
-		},
+func writeMessage(logger logging.Logger, cfg config.Configuration) {
+	section, ok := cfg.GetSection("main")
+	if ok {
+		message, ok := section.GetString("message")
+		if ok {
+			logger.Info(message)
+		} else {
+			logger.Panic("Cannot find configuration setting")
+		}
+	} else {
+		logger.Panic("Config section not found")
 	}
-	res, found := config.Get("database:credentials:username")
-	fmt.Println(res, found)
+}
+
+func main() {
+	var cfg config.Configuration
+	var err error
+	cfg, err = config.Load("config/config.json")
+	if err != nil {
+		panic(err)
+	}
+	var logger logging.Logger = logging.NewDefaultLogger(cfg)
+	writeMessage(logger, cfg)
 
 }
